0e5728bc14eabe7238a231d6716b6886c4285e91
CEMAL YILDIRIM <bystrong@technovadi.com>
2024-01-28 22:33:43 +0300

    `HomeScreen` => **BannerCarousel**

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import { ScrollView } from "react-native";
 
 import HeaderMain from "../../components/HeaderMain";
+import BannerCarousel from "../../components/BannerCarousel";
 
 const HomeScreen: React.FC = () => {
   return (
-    <ScrollView>
+    <ScrollView
+      stickyHeaderIndices={[0]}
+      style={{
+        backgroundColor: "#f5f5f5",
+      }}
+    >
       <HeaderMain />
+      <BannerCarousel />
     </ScrollView>
   );
 };
